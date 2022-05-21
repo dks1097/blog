ba98d66012cc8d26582580c6d786a1c1738f86b6
dks1097 <carlos.d.t.m19@gmail.com>
2022-05-21 02:04:53 +0100

    Initial Context dev complete

---
+++ src/context/BlogContext.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({ children }) => {
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  const addBlogPost = () => {
+    setBlogPosts([
+      ...blogPosts,
+      { title: `Blog Post #${blogPosts.length + 1}` },
+    ]);
+  };
+
+  const editBlogPost = () => {};
+  const deleteBlogPost = () => {};
+  return (
+    <BlogContext.Provider
+      value={{ data: blogPosts, addBlogPost, editBlogPost, deleteBlogPost }}
+    >
+      {children}
+    </BlogContext.Provider>
+  );
+};
+
+export default BlogContext;
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,21 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet, Text, View, FlatList, Button } from "react-native";
+import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
+  const { data, addBlogPost } = useContext(BlogContext);
+
   return (
     <View>
       <Text>Index Screen</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
