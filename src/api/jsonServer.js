979c32e11b2bdda42d3b0563e85f94aacf35b73a
dks1097 <carlos.d.t.m19@gmail.com>
2022-06-12 20:56:36 +0100

    Api integration

---
+++ src/api/jsonServer.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://248c-2001-8a0-e559-f200-e04e-d2a4-32f-bc66.eu.ngrok.io',
+
+});
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import {
   StyleSheet,
   Text,
@@ -12,7 +12,18 @@ import { Context } from "../context/BlogContext";
 import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = ({ navigation }) => {
-  const { state, deleteBlogPost } = useContext(Context);
+  const { state, deleteBlogPost, getBlogPosts } = useContext(Context);
+
+  useEffect(() => {
+    getBlogPosts();
+
+    const listener = navigation.addListener("didFocus", () => {
+      getBlogPosts();
+    });
+    return()=>{
+      listener.remove();
+    } // clean up the listener
+  }, []);
 
   return (
     <View>
